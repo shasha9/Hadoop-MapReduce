02a1dde4b01515f5b5e8c7e9cbadda0bcfd69ea7
Shasha Sami <shasha.sami91199@gmail.com>
2020-08-05 23:27:42 +0530

    Update Map.java

--- Word Count/src/Map.java
+++ Word Count/src/Map.java
@@ -5,7 +5,7 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class Map extends Mapper<Object, Text,Text, IntWritable>
+public class Map extends Mapper<Object, Text,Text,IntWritable>
 {
     private final static IntWritable one = new IntWritable(1);
     private Text word = new Text();
